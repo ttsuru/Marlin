c52fff3c734a7c898bb6346d811896fe0d080860
TSURU <tsuyoshi.tsurushima@gmail.com>
2016-10-31 18:04:10 +0900

    Add TEMP_SENSOR_BED

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -229,7 +229,7 @@
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
-#define TEMP_SENSOR_BED 0
+#define TEMP_SENSOR_BED 5
 
 // This makes temp sensor 1 a redundant sensor for sensor 0. If the temperatures difference between these sensors is to high the print will be aborted.
 //#define TEMP_SENSOR_1_AS_REDUNDANT
